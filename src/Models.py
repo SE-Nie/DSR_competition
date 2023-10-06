ae0a812b3acebb292c6b35d39aef3969da2bcf55
MK-Statistics <mk.statistics@example.org>
2023-10-06 12:39:25 +0200

    20231006_Leo: Further defined sklearn pipelines, including functions to change params

--- src/Models.py
+++ src/Models.py
@@ -2,6 +2,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.tree import DecisionTreeClassifier
+from typing import Literal
+
 # 20231005_Leo
 # This class is meant to calls several sklearn-pipelines. 
 # It is designed to be initialized and used in the main.ipynb.
@@ -31,7 +33,7 @@ class Models:
                             remainder='passthrough'  # Pass columns not specified to the next step
                         )),
     
-                        ('estimator', DecisionTreeClassifier(random_state=42, max_depth=4, max_leaf_nodes=1000))   
+                        ('estimator', DecisionTreeClassifier(random_state=42, max_depth=24, max_leaf_nodes=1000))   
                     ],verbose=True)
             
         elif self.model == "XGBoost":
@@ -50,12 +52,19 @@ class Models:
         self.pipe.fit(X,y)
         
     def predict(self,X):
-        """_summary_
-
-        Args:
-            X (_type_): _description_
         """
+        This method calls the .predict()-function of the sklearn pipelinel.
+        Args:
+            X (pandas DataFrame): Data to predict (features)
+        """
+        return self.pipe.predict(X)
 
-        
+    def define_estimator_params(self,**kwargs):
+        """_summary_
+        """
+        for i in kwargs:
+            s = f"estimator__{i}={kwargs[i]}"
+            print(s)
+            self.pipe.set_params(s)
         
     
