dc21ab40369f75aceb8a682c7ea41aa74bc48358
Michael Gerloff <michael.gerloff@fu-berlin.de>
2023-10-06 16:25:58 +0200

    how many buildings per geo id

--- src/data/data_imp.py
+++ src/data/data_imp.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
+
 def import_data():
-    '''
+    """
     Function to import the dataset files for the Earthquake competition.
     Returns dataframes in following order:
         train_values
         train_labels
-        test_values 
-    '''
-    train_values = pd.read_csv('./data/raw/train_values.csv')
-    train_labels = pd.read_csv('./data/raw/train_labels.csv')
-    test_values = pd.read_csv('./data/raw/test_values.csv')
+        test_values
+    """
+    train_values = pd.read_csv("./data/raw/train_values.csv")
+    train_labels = pd.read_csv("./data/raw/train_labels.csv")
+    test_values = pd.read_csv("./data/raw/test_values.csv")
     return train_values, train_labels, test_values
