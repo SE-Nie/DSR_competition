037d4cc8a5f6a48e735717a9225d765f59ee4412
MK-Statistics <mk.statistics@example.org>
2023-10-06 13:44:32 +0200

    20231006_Leo: Finalized the pipeline for DecisionTreeClassifier

--- src/Models.py
+++ src/Models.py
@@ -2,6 +2,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import f1_score
 from typing import Literal
 
 # 20231005_Leo
@@ -24,16 +25,19 @@ class Models:
         self.model = model
         self.columns_to_encode = columns_to_encode
         
-        if self.model == "DecisionTree":
-            self.pipe = Pipeline([
-                    ('label_encoder', ColumnTransformer(
+        # The Encoder stays the same 
+        self.encoder = ('label_encoder', ColumnTransformer(
                         transformers=[
-                            ('label_encode', OrdinalEncoder(), self.columns_to_encode)
+                            ('label_encode', OrdinalEncoder(), columns_to_encode)
                             ],
                             remainder='passthrough'  # Pass columns not specified to the next step
-                        )),
-    
-                        ('estimator', DecisionTreeClassifier(random_state=42, max_depth=24, max_leaf_nodes=1000))   
+                        ))
+        
+        # Define the pipelines
+        if self.model == "DecisionTree":
+            self.pipe = Pipeline([
+                    self.encoder,
+                    ('estimator', DecisionTreeClassifier(random_state=42, max_depth=24, max_leaf_nodes=1000))   
                     ],verbose=True)
             
         elif self.model == "XGBoost":
@@ -41,7 +45,17 @@ class Models:
         
         elif self.model == "LightGBM":
             pass
+    
+    def set_estimator_params(self,**kwargs):
+        """
+        Sets the respective parameters of the esitmators within the pipeline.
+                Args:
+            kwargs (dict): Dict for params (e.g. random_state=42)
+        """
+        self.pipe.named_steps['estimator'].set_params(**kwargs)
         
+    # Fit, predict and get the f1-score
+    
     def fit(self,X,y):
         """
         This method calls the .fit()-function of the sklearn pipelinel.
@@ -51,20 +65,25 @@ class Models:
         """
         self.pipe.fit(X,y)
         
-    def predict(self,X):
+    def predict(self,X,get_score=False):
         """
         This method calls the .predict()-function of the sklearn pipelinel.
         Args:
             X (pandas DataFrame): Data to predict (features)
+        Returns:
+            numpy array: predicted values
         """
         return self.pipe.predict(X)
 
-    def define_estimator_params(self,**kwargs):
-        """_summary_
+    def get_f1_score(self,X,y):
         """
-        for i in kwargs:
-            s = f"estimator__{i}={kwargs[i]}"
-            print(s)
-            self.pipe.set_params(s)
-        
+        Returns the F1-micro-score as calculated by sklearn.metrics.
+        Args:
+            X (pandas DataFrame): X_test
+            y (pandas DataFrame): y_test
+        Returns:
+            float: f1-micro-score
+        """
+        predicted = self.pipe.predict(X)
+        return f1_score(y, predicted, average='micro')
     
